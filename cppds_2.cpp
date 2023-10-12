737202d1881220163498b78548eb277fdbeac330
Nishika-9 <142077540+Nishika-9@users.noreply.github.com>
2023-10-12 14:53:35 +0530

    Add files via upload

---
+++ cppds_2.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    int first,second,add,sub,multiply,divide,divide2;
+    first=5;
+    second=4;
+
+    add=first+second;
+    sub=first-second;
+    multiply=first*second;
+    divide=first/second;
+    divide2=first%second;
+
+    cout<<add<<endl;
+    cout<<sub<<endl;
+    cout<<multiply<<endl;
+    cout<<divide<<endl;
+    cout<<divide2<<endl;
+    
+    return 0;
+}
