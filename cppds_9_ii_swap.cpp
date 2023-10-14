fb5f8a98be1663341cd2de2ec4dacc980dda2a54
Nishika-9 <142077540+Nishika-9@users.noreply.github.com>
2023-10-14 09:05:18 +0530

    Add files via upload

---
+++ cppds_9_ii_swap.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int a,b;
+    a=10;
+    b=5;
+    cout<<"before swap"<<endl<<a<<endl<<b<<endl;
+    swap(a,b);
+    cout<<"after swap"<<endl<<a<<endl<<b<<endl;
+
+    return 0;
+}
